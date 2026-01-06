b38d1fc24d5a049e3ce7bad65fce1661c9a2113b
sjstardellis <sjstardellis@gmail.com>
2026-01-06 18:29:22 -0500

    Initial commit, setting up project

---
+++ src/main/java/com/sjbank/sjbankbackend/SjbankBackendApplication.java
@@ -0,0 +1,12 @@
+package com.sjbank.sjbankbackend;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjbankBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SjbankBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sjbank/sjbankbackend/SjbankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjbank.sjbankbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjbankBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
